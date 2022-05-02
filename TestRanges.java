7f348194164554ba5501c4b349684b35abae97d0
jomau05 <jomau05@gmail.com>
2022-05-02 15:44:17 -0500

    Pruebsa en TestRanges.java

--- TestRanges.java
+++ TestRanges.java
@@ -20,5 +20,36 @@ public class TestRanges {
         Interval[] expected = { new Interval(1.0, 100.0) };
         assertThat (ranges, contains(expected)); // checks ranges.iterator()
     }
+    
+    @Test
+    public void testRangesSubtract1() {
+        Ranges ranges = new Ranges(1.0, 100.0);
+        Interval toRemove = new Interval(1.0, 100.0);
+        ranges.subtract(toRemove);
+        assertThat (ranges.remaining.size(), is(0)); // checks ranges.iterator()
+    }
 
+    @Test
+    public void testRangesSubtract2() {
+        Ranges ranges = new Ranges(1.0, 100.0);
+        Interval toRemove = new Interval(1.000, 1.0);
+        ranges.subtract(toRemove);
+        Interval[] expected = { new Interval(1.0, 100.0) };
+        assertThat (ranges, contains(expected)); // checks ranges.iterator()
+    }
+    
+    @Test
+    public void testRangesToString() {
+        Ranges ranges = new Ranges(1.0, 100.0);
+        String expected = "[(1.0,100.0)]";
+        assertThat (ranges.toString(), is(expected)); // checks ranges.iterator()
+    }
+    
+    @Test
+    public void testRangesIterator() {
+        Ranges ranges = new Ranges(1.0, 100.0);
+        Interval[] expected = { new Interval(1.0, 100.0) };
+        assertThat (ranges.iterator().hasNext(), is(true)); // checks ranges.iterator()
+    }
+    
 }
